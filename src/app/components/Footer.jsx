f03e645c6b5dba9479962c8b7cab94944cf0f913
a1shuvo <a1shuvosaha@gmail.com>
2025-07-20 19:46:45 +0600

    Initialized basic next js app with Navbar footer and content

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="h-12 bg-amber-100 text-sm flex items-center justify-center">
+      &copy; Awesome Next js footer
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="h-16 bg-amber-100 flex justify-center items-center">
+      <ul className="flex gap-6 text-xl">
+        <li>Home</li>
+        <li>Services</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
